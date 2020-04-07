eb1101e9d6caf6bcd1de825a2fea92eaf1cffec7
Spark <79936503+spa5k@users.noreply.github.com>
2020-04-07 15:31:31 +0000

    Syncing - Group Anagrams

--- 0049 Group Anagrams/Solution.js
+++ 0049 Group Anagrams/Solution.js
@@ -3,14 +3,41 @@
  * @return {string[][]}
  */
 
-
-
 var groupAnagrams = function (strs) {
-      const map = {};
-      const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101];
-      strs.forEach(str => {
-        let prod = str.split("").reduce((r, c) => r * primes[c.charCodeAt() - 97], 1);
-        map[prod] ? map[prod].push(str) : map[prod] = [str];
-      });
-      return Object.values(map);
-    };
+  const map = {};
+  const primes = [
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    43,
+    47,
+    53,
+    59,
+    61,
+    67,
+    71,
+    73,
+    79,
+    83,
+    89,
+    97,
+    101,
+  ];
+  strs.forEach((str) => {
+    let prod = str
+      .split('')
+      .reduce((r, c) => r * primes[c.charCodeAt() - 97], 1);
+    map[prod] ? map[prod].push(str) : (map[prod] = [str]);
+  });
+  return Object.values(map);
+};
