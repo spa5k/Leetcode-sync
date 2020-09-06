1404117b4350d6569de5f5de753f2ada7bcf1aeb
Spark <79936503+spa5k@users.noreply.github.com>
2020-09-06 19:50:11 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (javascript)

--- problems/longest_substring_without_repeating_characters/solution.js
+++ problems/longest_substring_without_repeating_characters/solution.js
@@ -2,18 +2,15 @@
  * @param {string} s
  * @return {number}
  */
-const lengthOfLongestSubstring = (s) => {
-  let temp = [];
-  let maxLength = 0;
-
-  for (let i = 0; i < s.length; i++) {
-    if (temp.indexOf(s[i]) === -1) {
-      temp.push(s[i]);
-      maxLength = Math.max(temp.length, maxLength);
-    } else {
-      temp = temp.slice(temp.indexOf(s[i]) + 1, temp.length);
-      temp.push(s[i]);
+var lengthOfLongestSubstring = function(s) {
+    let n = s.length, ans = 0;
+    let index = new Array(26);
+    for(let j=0,i=0;j<n;j++) {
+        if(index[s.charAt(j)]){
+            i = Math.max(index[s.charAt(j)], i);
+        }
+        ans = Math.max(ans, j-i+1);
+        index[s.charAt(j)] = j+1;
     }
-  }
-  return maxLength;
-};
+    return ans;
+}
