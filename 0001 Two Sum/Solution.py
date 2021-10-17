beed4eb1c8cc1789dd5a731367d3ec81c385fd8c
Spark <79936503+spa5k@users.noreply.github.com>
2021-10-17 10:41:42 +0000

    Syncing - Two Sum

--- 0001 Two Sum/Solution.py
+++ 0001 Two Sum/Solution.py
@@ -1,14 +1,8 @@
 class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        h = {}
-        for i, num in enumerate(nums):
-            n = target - num
-            if n not in h:
-                h[num] = i
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = dict()
+        for i in range(len(nums)):
+            if target - nums[i] in hash_map:
+                return [hash_map[target-nums[i]], i]
             else:
-                return [h[n], i]
+                hash_map[nums[i]] = i
