8812619adeb45fabc413d22501eda7d319b5ead5
Spark <79936503+spa5k@users.noreply.github.com>
2021-10-24 07:49:25 +0000

    Sync LeetCode submission - Find the Duplicate Number (python3)

--- problems/find_the_duplicate_number/solution.py
+++ problems/find_the_duplicate_number/solution.py
@@ -1,10 +1,16 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        for num in nums:
-            absValue = abs(num)
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
 
-            if nums[absValue-1] < 0:
-                return absValue
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
 
-            nums[absValue-1] *= -1
-        return -1
+            if slow == slow2:
+                return slow
