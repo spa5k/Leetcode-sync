5a2e40b32903808244bb2b96b622b7f771b72320
Spark <79936503+spa5k@users.noreply.github.com>
2021-10-24 13:10:46 +0000

    Sync LeetCode submission - Remove Linked List Elements (python3)

--- problems/remove_linked_list_elements/solution.py
+++ problems/remove_linked_list_elements/solution.py
@@ -5,14 +5,16 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        prev, curr = None, head
-        while curr:
-            if curr.val == val:  # cases 1-3
-                if prev:  # cases 1-2
-                    prev.next = curr.next
-                else:  # case 3
-                    head = curr.next
-                curr = curr.next  # for all cases
-            else:  # case 4
-                prev, curr = curr, curr.next
+        current = head
+        prev = None
+        while current:
+            if current.val == val:
+                if current == head:
+                    current = head = head.next
+                else:
+                    prev.next = current.next
+                    current = current.next
+            else:
+                prev = current
+                current = current.next
         return head
