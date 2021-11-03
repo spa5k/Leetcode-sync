eaaca29d570fd2b5daae5ee58e56cad7b82382e2
Spark <79936503+spa5k@users.noreply.github.com>
2021-11-03 12:27:47 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (python3)

--- problems/search_in_rotated_sorted_array/solution.py
+++ problems/search_in_rotated_sorted_array/solution.py
@@ -1,24 +1,23 @@
-from typing import List
-
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)-1
+        left = 0
+        right = len(nums)-1
 
-        while l <= r:
-            mid = (l+r)//2
+        while left <= right:
+            middle = (left+right)//2
 
-            if nums[mid] == target:
-                return mid
-            # This is strictly increasing sub array
-            elif nums[mid] >= nums[l]:
-                if target <= nums[mid] and target >= nums[l]:
-                    r = mid-1
+            if target == nums[middle]:
+                return middle
+
+            if nums[left] <= nums[middle]:
+                # if target is greater than middle, or target is less than left most value.
+                if target > nums[middle] or target < nums[left]:
+                    left = middle+1
                 else:
-                    l = mid+1
+                    right = middle-1
             else:
-                if target >= nums[mid] and target <= nums[r]:
-                    l = mid+1
+                if target < nums[middle] or target > nums[right]:
+                    right = middle-1
                 else:
-                    r = mid-1
+                    left = middle+1
         return -1
