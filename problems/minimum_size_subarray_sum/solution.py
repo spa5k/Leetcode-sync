6f30192aa140dac42ece499334a29e574489a48e
Spark <79936503+spa5k@users.noreply.github.com>
2021-12-04 18:30:44 +0000

    Sync LeetCode submission - Minimum Size Subarray Sum (python3)

--- problems/minimum_size_subarray_sum/solution.py
+++ problems/minimum_size_subarray_sum/solution.py
@@ -2,16 +2,16 @@ class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         left = 0
         total = 0
+        current = nums[0]
         min_length = len(nums)+1
 
         for right in range(len(nums)):
-            total += nums[right]
+            total = total+nums[right]
 
             while total >= target:
                 min_length = min(min_length, right-left+1)
                 total = total-nums[left]
                 left += 1
-        if min_length <= len(nums):
-            return min_length
-        else:
-            return 0
+
+        return min_length if min_length <= len(nums)else 0
+
