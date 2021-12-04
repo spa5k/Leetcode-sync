af70eea990d67baa8b2f6d5a797549831c573169
Spark <79936503+spa5k@users.noreply.github.com>
2021-12-04 18:30:44 +0000

    Syncing - Minimum Size Subarray Sum

---
+++ 0209 Minimum Size Subarray Sum/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        total = 0
+        current = nums[0]
+        min_length = len(nums)+1
+
+        for right in range(len(nums)):
+            total = total+nums[right]
+
+            while total >= target:
+                min_length = min(min_length, right-left+1)
+                total = total-nums[left]
+                left += 1
+
+        return min_length if min_length <= len(nums)else 0
+
