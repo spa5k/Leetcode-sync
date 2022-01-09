462874c9ffb9d91c8a73434576aad93f5d2557bb
Spark <79936503+spa5k@users.noreply.github.com>
2022-01-09 08:15:20 +0000

    Syncing - Longest Substring Without Repeating Characters

--- 0003 Longest Substring Without Repeating Characters/Solution.py
+++ 0003 Longest Substring Without Repeating Characters/Solution.py
@@ -1,15 +1,12 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_set = set()
+        char_set=set()
         max_length = 0
-
-        left = 0
-
+        left =0
         for right in range(len(s)):
-            while s[right] in char_set:
-                char_set.remove(s[left])
-                left += 1
-
-            char_set.add(s[right])
-            max_length = max(max_length, right-left+1)
+                while s[right] in char_set:
+                    char_set.remove(s[left])
+                    left+=1
+                char_set.add(s[right])
+                max_length=max(max_length, right-left+1)
         return max_length
