022951beab884805bd4d77cc21f0389b9939e44f
Spark <79936503+spa5k@users.noreply.github.com>
2022-04-11 10:33:10 +0000

    Syncing - Best Time to Buy and Sell Stock

--- 0121 Best Time to Buy and Sell Stock/Solution.py
+++ 0121 Best Time to Buy and Sell Stock/Solution.py
@@ -1,18 +1,19 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        left = 0
-        right = 1
-        max_profit = 0
+  def maxProfit(self, prices: List[int]) -> int:
+      left = 0
+      right = 1
 
-        for i in range(len(prices)-1):
-            current_profit = prices[right]-prices[left]
+      max_profit = 0
+      for i in range(len(prices) - 1):
+          cur_left = prices[left]
+          cur_right = prices[right]
+          cur_profit = cur_right - cur_left
 
-            if prices[right] < prices[left]:
-                left = right
-                right += 1
-                continue
+          if cur_left > cur_right:
+              left = right
+              right += 1
+              continue
+          max_profit = max(max_profit, cur_profit)
 
-            max_profit = max(current_profit, max_profit)
-            right += 1
-        return max_profit
-        
+          right += 1
+      return max_profit
