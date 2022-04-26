a604e053122fdb64ee92b08a0bb860b1a0636e11
Spark <79936503+spa5k@users.noreply.github.com>
2022-04-26 18:57:45 +0000

    Syncing - Koko Eating Bananas

--- 0907 Koko Eating Bananas/Solution.py
+++ 0907 Koko Eating Bananas/Solution.py
@@ -1,19 +1,16 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l = 1
-        r = max(piles)
-
+        l, r = 1, max(piles)
         res = r
         while l <= r:
-            k = (l+r)//2
+            k = (l + r) // 2
             hours = 0
             for p in piles:
-                hours += math.ceil(p/k)
+                hours += math.ceil(p / k)
 
             if hours <= h:
                 res = min(res, k)
-                r = k-1
+                r = k - 1
             else:
-                l = k+1
+                l = k + 1
         return res
-
