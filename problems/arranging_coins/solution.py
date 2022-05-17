3fbeb51380b0bc0dc377cb7eb73f5ed6c3788d4b
Spark <79936503+spa5k@users.noreply.github.com>
2022-05-17 13:06:49 +0000

    Sync LeetCode submission - Arranging Coins (python3)

--- problems/arranging_coins/solution.py
+++ problems/arranging_coins/solution.py
@@ -1,7 +1,13 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        k = total = 1
-        while total <= n:
-            k += 1
-            total += k
-        return k-1
+        l, r = 0, n
+        while l <= r:
+            mid = (l+r)//2
+            current = mid*(mid+1)//2
+            if current == n:
+                return mid
+            elif current <= n:
+                l = mid+1
+            else:
+                r = mid-1
+        return r
