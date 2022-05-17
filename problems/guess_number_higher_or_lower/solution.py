55a644fa236717b0826339bc7a163073408a7c15
Spark <79936503+spa5k@users.noreply.github.com>
2022-05-17 13:13:37 +0000

    Sync LeetCode submission - Guess Number Higher or Lower (python3)

---
+++ problems/guess_number_higher_or_lower/solution.py
@@ -0,0 +1,22 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l, r = 0, n
+
+        while l <= r:
+            mid = (l+r)//2
+
+            res = guess(mid)
+
+            if res == 0:
+                return mid
+            elif res < 0:
+                r = mid-1
+            else:
+                l = mid+1
