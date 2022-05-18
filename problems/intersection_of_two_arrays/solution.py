bad617b0eb86d2f57a861df43c14d861a40459cb
Spark <79936503+spa5k@users.noreply.github.com>
2022-05-18 17:49:57 +0000

    Sync LeetCode submission - Intersection of Two Arrays (python3)

---
+++ problems/intersection_of_two_arrays/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) < len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        res = []
+        nums1 = sorted(nums1)
+        nums2 = set(nums2)
+
+        for i in nums2:
+            l, r = 0, len(nums1)-1
+
+            while l <= r:
+                m = (l+r)//2
+                if nums1[m] == i:
+                    res.append(i)
+                    break
+                elif nums1[m] < i:
+                    l = m+1
+                else:
+                    r = m-1
+        return res
