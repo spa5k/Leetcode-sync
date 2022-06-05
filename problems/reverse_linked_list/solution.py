67d4282d8b5b3bb945ccc96d477911546f24e88b
Spark <79936503+spa5k@users.noreply.github.com>
2022-06-05 13:28:56 +0000

    Sync LeetCode submission - Reverse Linked List (python3)

--- problems/reverse_linked_list/solution.py
+++ problems/reverse_linked_list/solution.py
@@ -1,19 +1,16 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        current_node = head
-
-        prev_node = None
-
+        current_node=head
+        
+        reverse=None
         while current_node:
-            next = current_node.next
-            current_node.next = prev_node
-            prev_node = current_node
-            current_node = next
-        return prev_node
+            next=current_node.next
+            current_node.next=reverse
+            reverse=current_node
+            current_node=next
+        return reverse
