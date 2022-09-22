b5c9ae6e6571071ebe1773d7653c2dde56d397cd
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-22 18:59:15 +0000

    Sync LeetCode submission - Task Scheduler (cpp)

--- problems/task_scheduler/solution.cpp
+++ problems/task_scheduler/solution.cpp
@@ -1,39 +1,24 @@
 class Solution
 {
 public:
-  int leastInterval(vector<char> &tasks, int k)
+  int leastInterval(vector<char> &tasks, int n)
   {
-    unordered_map<char, int> hash;
-    for (int i = 0; i < tasks.size(); i++)
-      hash[tasks[i]]++;
-
-    priority_queue<int> pq;
-
-    for (auto it : hash)
-      pq.push(it.second);
-
-    int timer = 0;
-
-    while (!pq.empty())
+    unordered_map<char, int> mp;
+    int count = 0;
+    for (auto e : tasks)
     {
-      vector<int> temp;
-      for (int i = 0; i <= k; i++)
-      {
-        if (!pq.empty())
-        {
-          temp.push_back(pq.top());
-          pq.pop();
-        }
-      }
-
-      for (int i = 0; i < temp.size(); i++)
-      {
-        if (--temp[i] > 0)
-          pq.push(temp[i]);
-      }
-
-      timer += pq.empty() ? temp.size() : k + 1;
+      mp[e]++;
+      count = max(count, mp[e]);
     }
-    return timer;
+
+    int ans = (count - 1) * (n + 1);
+    for (auto e : mp)
+    {
+      if (e.second == count)
+      {
+        ans++;
+      }
+    }
+    return max((int)tasks.size(), ans);
   }
 };
