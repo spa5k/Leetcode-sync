8fef0eef00b2913c16c5c9a8819a37103c246165
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-27 17:40:33 +0000

    Syncing - Clone Graph

---
+++ 0133 Clone Graph/Solution.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+  Node *cloner(Node *cur, unordered_map<Node *, Node *> &mp)
+  {
+    if (mp.find(cur) != mp.end())
+    {
+      return mp[cur];
+    }
+    Node *clone = new Node(cur->val);
+    mp[cur] = clone;
+    for (auto &&nei : cur->neighbors)
+    {
+      clone->neighbors.push_back(cloner(nei, mp));
+    }
+    return clone;
+  }
+  Node *cloneGraph(Node *node)
+  {
+    unordered_map<Node *, Node *> mp;
+    if (node == nullptr)
+    {
+      return nullptr;
+    }
+    // if (node->neighbors.size() == 0)
+    // {
+    //   Node *clone = new Node(node->val);
+    //   return clone;
+    // }
+    return cloner(node, mp);
+  }
+};
