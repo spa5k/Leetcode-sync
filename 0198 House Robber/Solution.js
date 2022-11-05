83ccb3c0a78913b5acded1576ab56d7d0e42c649
Spark <79936503+spa5k@users.noreply.github.com>
2022-11-05 16:59:38 +0000

    Syncing - House Robber

--- 0198 House Robber/Solution.js
+++ 0198 House Robber/Solution.js
@@ -5,9 +5,8 @@
 const rob = (nums) => {
   let rob1 = 0;
   let rob2 = 0;
-
-  for (const element of nums) {
-    let temp = Math.max(element + rob1, rob2);
+  for (const num of nums) {
+    const temp = Math.max(num + rob1, rob2);
     rob1 = rob2;
     rob2 = temp;
   }
