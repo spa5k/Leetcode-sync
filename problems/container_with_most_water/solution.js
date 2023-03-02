2fb3a3c92dba1d308dfd2c75f84dd16e01922bbf
Spark <79936503+spa5k@users.noreply.github.com>
2023-03-02 19:46:25 +0000

    Sync LeetCode submission - Container With Most Water (javascript)

--- problems/container_with_most_water/solution.js
+++ problems/container_with_most_water/solution.js
@@ -1,14 +1,15 @@
-/**
- * @param {number[]} height
- * @return {number}
- */
-var maxArea = function (H) {
-  let ans = 0,
-    i = 0,
-    j = H.length - 1;
-  while (i < j) {
-    ans = Math.max(ans, Math.min(H[i], H[j]) * (j - i));
-    H[i] <= H[j] ? i++ : j--;
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxVol = Math.min(height[left], height[right]) * (right - left);
+  while (left < right) {
+    let curr = Math.min(height[left], height[right]) * (right - left);
+    maxVol = Math.max(curr, maxVol);
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
   }
-  return ans;
+  return maxVol;
 };
