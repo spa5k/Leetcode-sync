0fb08d733e9ee4c3f3f65bf911447b5cb152037d
Spark <79936503+spa5k@users.noreply.github.com>
2023-03-12 16:49:45 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- problems/number_of_islands/solution.cpp
+++ problems/number_of_islands/solution.cpp
@@ -4,17 +4,17 @@ public:
   void dfs(int i, int j, vector<vector<char>> &grid)
   {
     if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0')
+    {
       return;
+    }
     grid[i][j] = '0';
     dfs(i + 1, j, grid);
     dfs(i - 1, j, grid);
     dfs(i, j + 1, grid);
     dfs(i, j - 1, grid);
   }
-
   int numIslands(vector<vector<char>> &grid)
   {
-
     int islands = 0;
     for (int i = 0; i < grid.size(); i++)
     {
