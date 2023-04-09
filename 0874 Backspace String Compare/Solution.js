1d50b73ed595f5b55d004d23c57625d327d800bf
Spark <79936503+spa5k@users.noreply.github.com>
2023-04-09 10:12:29 +0000

    Syncing - Backspace String Compare

--- 0874 Backspace String Compare/Solution.js
+++ 0874 Backspace String Compare/Solution.js
@@ -1,14 +1,37 @@
-/**
- * @param {string} S
- * @param {string} T
- * @return {boolean}
- */
-const backspaceCompare = (S, T) => {
-    const buildString = str => Array.from(str).reduce((acc, cur) => {
-        if(cur === '#') {
-            return acc.slice(0, acc.length - 1);
-        }
-        return [...acc, cur];
-    }, []).join('');
-    return buildString(S) === buildString(T)
+var backspaceCompare = function (s, t) {
+  const s1 = [];
+  const s2 = [];
+
+  for (const i of s) {
+    if (i === "#") {
+      if (s1.length > 0) {
+        s1.pop();
+      }
+    } else {
+      s1.push(i);
+    }
+  }
+
+  for (const i of t) {
+    if (i === "#") {
+      if (s2.length > 0) {
+        s2.pop();
+      }
+    } else {
+      s2.push(i);
+    }
+  }
+  if (s1.length !== s2.length) {
+    return false;
+  }
+  while (s1.length > 0) {
+    const t1 = s1[s1.length - 1];
+    const t2 = s2[s1.length - 1];
+    s1.pop();
+    s2.pop();
+    if (t1 !== t2) {
+      return false;
+    }
+  }
+  return true;
 };
