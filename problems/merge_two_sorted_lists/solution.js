67405f7937f9583f09557cfdbbbdb71c1ced41d8
Spark <79936503+spa5k@users.noreply.github.com>
2023-04-12 07:16:21 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (javascript)

--- problems/merge_two_sorted_lists/solution.js
+++ problems/merge_two_sorted_lists/solution.js
@@ -1,40 +1,34 @@
 /**
  * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
  * }
  */
 /**
- * @param {ListNode} l1
- * @param {ListNode} l2
+ * @param {ListNode} list1
+ * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-  var dummyHead = new ListNode();
-  var cur = dummyHead;
+var mergeTwoLists = function (l1, l2) {
+  let prehead = new ListNode(-1);
+  let prev = prehead;
 
   while (l1 && l2) {
     if (l1.val < l2.val) {
-      cur.next = l1;
+      prev.next = l1;
       l1 = l1.next;
     } else {
-      cur.next = l2;
+      prev.next = l2;
       l2 = l2.next;
     }
-    cur = cur.next;
+    prev = prev.next;
   }
 
-  while (l1) {
-    cur.next = l1;
-    l1 = l1.next;
-    cur = cur.next;
+  if (l1 == null && l2 !== null) {
+    prev.next = l2;
+  } else {
+    prev.next = l1;
   }
-
-  while (l2) {
-    cur.next = l2;
-    l2 = l2.next;
-    cur = cur.next;
-  }
-  return dummyHead.next;
+  return prehead.next;
 };
