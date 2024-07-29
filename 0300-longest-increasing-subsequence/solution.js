e402dfafe218697910142f5ae87f45a9dde30710
Spark <79936503+spa5k@users.noreply.github.com>
2024-07-29 11:04:41 +0000

    Sync LeetCode submission Runtime - 151 ms (33.97%), Memory - 51.2 MB (27.15%)

--- 0300-longest-increasing-subsequence/solution.js
+++ 0300-longest-increasing-subsequence/solution.js
@@ -5,15 +5,17 @@
 var lengthOfLIS = function (nums) {
     if (nums.length === 0) return 0;
 
-    let dp = new Array(nums.length).fill(1);
+    const dp = new Array(nums.length).fill(1);
 
-    for (let i = nums.length - 1; i >= 0; i--) {
+    let max = 1;
+
+    for (let i = nums.length; i >= 0; i--) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] < nums[j]) {
                 dp[i] = Math.max(dp[i], 1 + dp[j]);
+                max = Math.max(max, dp[i]);
             }
         }
     }
-
-    return Math.max(...dp);
+    return max;
 };
