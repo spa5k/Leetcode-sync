c2f752275d123aa67c060a3fe42de50324726f8f
Spark <79936503+spa5k@users.noreply.github.com>
2024-07-29 14:45:11 +0000

    Sync LeetCode submission Runtime - 47 ms (77.55%), Memory - 48.5 MB (80.13%)

---
+++ 0070-climbing-stairs/solution.js
@@ -0,0 +1,11 @@
+const climbStairs = (n) => {
+    let one = 1;
+    let two = 1;
+
+    for (let i = 0; i < n - 1; i++) {
+        const temp = one;
+        one += two;
+        two = temp;
+    }
+    return one;
+};
