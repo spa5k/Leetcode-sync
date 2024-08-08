cc7ba936115502c830bb30aeb8ebe294c830ccab
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-08 11:18:03 +0000

    Sync LeetCode submission Runtime - 51 ms (73.92%), Memory - 49.8 MB (26.72%)

--- 0062-unique-paths/solution.js
+++ 0062-unique-paths/solution.js
@@ -3,14 +3,25 @@
  * @param {number} n
  * @return {number}
  */
-var uniquePaths = function (m, n, memo = {}) {
-    const key = m + ',' + n;
-    if (memo[key]) return memo[key];
-    if (m === 0 || n === 0) return 0;
+var uniquePaths = function (m, n) {
+    const dp = Array(m + 1)
+        .fill()
+        .map(() => Array(n + 1).fill(0));
 
-    if (m === 1 && n === 1) return m * n;
+    dp[1][1] = 1;
 
-    const res = uniquePaths(m - 1, n, memo) + uniquePaths(m, n - 1, memo);
-    memo[key] = res;
-    return res;
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            const current = dp[i][j];
+            if (i + 1 <= m) {
+                dp[i + 1][j] += current;
+            }
+
+            if (j + 1 <= n) {
+                dp[i][j + 1] += current;
+            }
+        }
+    }
+
+    return dp[m][n];
 };
